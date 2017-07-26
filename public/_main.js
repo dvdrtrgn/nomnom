b41e79fb0a0fc193239930499a15250e0964ceea
dvdrtrgn <dvdrtrgn@gmail.com>
2017-07-26 17:32:26 -0500

    add defcon helper

--- public/_main.js
+++ public/_main.js
@@ -20,6 +20,22 @@ requirejs.config({
 
 requirejs(['lib/cookie'], function (cookie) {
   _drt.cookie = cookie;
+  _drt.defcon = function (num) {
+    switch (num) {
+    case 1:
+      cookie.set('drt', 'notify/_notify,toplist/_toplist');
+      break;
+    case 2:
+      cookie.set('drt', 'notify/_notify');
+      break;
+    case 3:
+      cookie.set('drt', 'toplist/_toplist');
+      break;
+    default:
+      cookie.set('drt', '');
+    }
+    W.location.reload();
+  };
 
   var paths = cookie.get('drt');
   if (paths) requirejs(paths.split(','));
