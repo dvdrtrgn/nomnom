a5e55ff6e7bebd12bd5f41468ab594bf1cd9eeff
dvdrtrgn <dvdrtrgn@gmail.com>
2017-08-03 16:07:42 -0500

    refactor toplist

--- public/toplist/_toplist.js
+++ public/toplist/_toplist.js
@@ -16,73 +16,73 @@ define(['jqxtn', './hash', 'lib/endpoint',
   var Data = {};
 
   function dupeCard() {
-    var sels = '.gallery > .gallery-item, .possible-card-wrapper .possible-card';
-    var card = $(sels);
-    if (card.length > 2) {
-      card = card.eq(2);
-    } else {
-      card = card.last();
-    }
-    var dupe = card.clone();
+    var card = $('.gallery > .gallery-item, .possible-card-wrapper .possible-card');
+    var dupe;
+
+    card = (card.length > 2) ? card.eq(2) : card.last();
+    dupe = card.clone();
 
     if (dupe.is('.toplist')) {
-      dupe = card;
+      dupe = card; // already there! (for whatever reason)
     } else {
       dupe.addClass('toplist').insertAfter(card);
     }
     return dupe.empty();
   }
 
+  function genUrl(ele) {
+    var data = ele.data(Nom);
+    var url = _drt.site;
+
+    url += 'search-results/' + Hash.search(data.filter);
+    url += encodeURIComponent(Hash.research(data.term));
+
+    return url;
+  }
+
   function trigFilter(evt) {
     evt.preventDefault();
     var ele = $(this);
-    var dat = ele.data('Filter');
-    var url = _drt.site;
 
-    url += 'search-results/' + Hash.search(dat.filter);
-    url += encodeURIComponent(Hash.research(dat.term));
-    W.location = url;
+    W.location = genUrl(ele);
+  }
+
+  function makeLine(arr, filter) {
+    var line = $('<li>');
+    var obj = {
+      filter: filter,
+      term: arr[0],
+      count: arr[1],
+    };
+    line.html('<a href="#">' + Hash.search(obj.term) + ' (' + obj.count + ' posts)</a>');
+    line.on('click', trigFilter);
+    return line.data(Nom, obj);
+  }
+
+  function makeArticle(obj) {
+    var div = $('<article>');
+    var head = $('<b>').html(obj.title);
+    var list = $('<ol>');
+    obj.data.forEach(function (item) {
+      list.append(makeLine(item, obj.filter));
+    });
+    return div.append(head, list);
+  }
+
+  function transArray(arr) {
+    var init = arr.shift();
+    var obj = {
+      title: init[0],
+      filter: init[1],
+      data: arr,
+    };
+    return makeArticle(obj);
   }
 
   function data2elem(data) {
 
-    function makeItem(arr, filter) {
-      var li = $('<li>');
-      var dat = {
-        filter: filter,
-        term: arr[0],
-        count: arr[1],
-      };
-
-      li.html('<a href="#">' + Hash.search(dat.term) + ' (' + dat.count + ' posts)</a>');
-      li.on('click', trigFilter).data('Filter', dat);
-      return li;
-    }
-
-    function makeArticle(obj) {
-      var h1 = $('<b>').html(obj.title);
-      var ol = $('<ol>');
-      var div = $('<article>').append(h1, ol);
-
-      obj.list.forEach(function (item) {
-        ol.append(makeItem(item, obj.filter));
-      });
-
-      return div;
-    }
-
-    function prepData(arr) {
-      var init = arr.shift();
-      var obj = {
-        title: init[0],
-        filter: init[1],
-        list: arr,
-      };
-      return makeArticle(obj);
-    }
-
-    data.cities = prepData(data.cities);
-    data.categories = prepData(data.categories);
+    data.cities = transArray(data.cities);
+    data.categories = transArray(data.categories);
 
     return data;
   }
